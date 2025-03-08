2118743d07e07e248f4db41a3c32dcd182599ac3
somil08 <Somilrathore0208@gmail.com>
2025-03-08 14:51:03 +0530

    v2 of brainly

--- brainly-frontent/src/components/CreateComonent.tsx
+++ brainly-frontent/src/components/CreateComonent.tsx
@@ -2,7 +2,9 @@ import { useRef } from "react";
 import { CrossIcon } from "../icons/CrossIcon";
 import { Button } from "./Button";
 import { Input } from "./Input";
-import { useState } from "react"; 
+import { useState } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 enum contentType {
   Youtube = "youtube",
   Twitter = "twitter",
@@ -12,64 +14,77 @@ export function CreatecComponent({ open, onClose }) {
   const titleRef = useRef<HTMLInputElement | null>(null);
   const linkRef = useRef<HTMLInputElement | null>(null);
   const [type, setType] = useState(contentType.Youtube);
-  function addContent() {
+ 
+ 
+  async function addContent() {
     const title = titleRef.current?.value;
     const link = linkRef.current?.value;
+
+    await axios.post(`${BACKEND_URL}/api/v1/content`,{
+      link,
+      title,
+      type
+    },{
+      headers : {
+        "Authorization" : localStorage.getItem("token")
+      }
+    })
   }
   return (
     <div>
       {open && (
-        <div
-          className="w-screen h-screen bg-slate-500 fixed top-0 left-0 
+        <div>
+          <div
+            className="w-screen h-screen bg-slate-500 fixed top-0 left-0 
         opacity-60 flex justify-center"
-        >
-          <div className="flex flex-col justify-center">
-            <span className="bg-white opacity-100 p-4 rounded">
-              <div className="flex justify-end">
-                <div onClick={onClose} className="cursor-pointer">
-                  <CrossIcon />
+          ></div>
+          <div
+            className="w-screen h-screen fixed top-0 left-0 
+         flex justify-center"
+          >
+            <div className="flex flex-col justify-center">
+              <span className="bg-white opacity-100 p-4 rounded">
+                <div className="flex justify-end">
+                  <div onClick={onClose} className="cursor-pointer">
+                    <CrossIcon />
+                  </div>
                 </div>
-              </div>
-              <div>
-                <Input reference={titleRef} placeholder={"Title"} />
-                <Input reference={linkRef} placeholder={"Link"} />
-              </div>
-              <div>
-                <h1>Type</h1>
-                <div className="flex gap-1 p-4">
-                  <Button
-                    text="Youtube"
-                    variant={
-                      type === contentType.Youtube ? "primary" : "secondary"
-                    }
-                    onClick={() => {
-                      setType(contentType.Youtube);
-                    }}
-                  ></Button>
-                  <Button
-                    text="Twitter"
-                    variant={
-                      type === contentType.Twitter ? "primary" : "secondary"
-                    }
-                    onClick={() => {
-                      setType(contentType.Twitter);
-                    }}
-                  ></Button>
-                  <Button
-                    text="Twitter"
-                    variant={
-                      type === contentType.Twitter ? "primary" : "secondary"
-                    }
-                    onClick={() => {
-                      setType(contentType.Twitter);
-                    }}
-                  ></Button>
+                <div>
+                  <Input reference={titleRef} placeholder={"Title"} />
+                  <Input reference={linkRef} placeholder={"Link"} />
                 </div>
-              </div>
-              <div className="flex justify-center">
-                <Button onClick={addContent} variant="primary" text="submit" />
-              </div>
-            </span>
+                <div>
+                  <h1>Type</h1>
+                  <div className="flex gap-1 justify-center pb-2">
+                    <Button
+                      text="Youtube"
+                      variant={
+                        type === contentType.Youtube ? "primary" : "secondary"
+                      }
+                      onClick={() => {
+                        setType(contentType.Youtube);
+                      }}
+                    ></Button>
+                    <Button
+                      text="Twitter"
+                      variant={
+                        type === contentType.Twitter ? "primary" : "secondary"
+                      }
+                      onClick={() => {
+                        setType(contentType.Twitter);
+                      }}
+                    ></Button>
+                  </div>
+                </div>
+                <div className="flex justify-center">
+                  <Button
+                    onClick={addContent}
+                    variant="primary"
+                    text="submit"
+                  />
+                </div>
+              </span>
+            </div>
           </div>
         </div>
       )}
@@ -79,4 +94,3 @@ export function CreatecComponent({ open, onClose }) {
 // function usestate(Youtube: contentType): [any, any] {
 //   throw new Error("Function not implemented.");
 // }
-
